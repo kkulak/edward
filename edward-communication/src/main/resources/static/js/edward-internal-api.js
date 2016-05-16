fd8d243f0c219f305545c4dc87dd6485ab6a5312
jacek-zyla <jacek.zyla@schibsted.pl>
2016-05-16 02:02:39 +0200

    Update libs. Chart skeleton.

--- edward-communication/src/main/resources/static/js/edward-internal-api.js
+++ edward-communication/src/main/resources/static/js/edward-internal-api.js
@@ -1,5 +1,5 @@
-
-var BASE_API_URL = "api/internal/";
+var HOST = "//localhost:8080";
+var BASE_API_URL = HOST + "/api/internal";
 
 var USER = "admin";
 var PASSWORD = "admin";
@@ -25,26 +25,26 @@ var httpPost = function(url, dataObject){
 }
 
 var getProject = function (id) {
-    return httpGet(BASE_API_URL + "project/" + id);
+    return httpGet(BASE_API_URL + "/project/" + id);
 }
 
 var getNumberOfConnectedVolunteers = function(){
-    return httpGet(BASE_API_URL + "volunteerCount/");
+    return httpGet(BASE_API_URL + "/volunteerCount/");
 }
 
 var getVersion = function(){
-    return httpGet(BASE_API_URL + "version/");
+    return httpGet(BASE_API_URL + "/version/");
 }
 
 var getJob = function (id) {
-    return httpGet(BASE_API_URL + "job/" + id);
+    return httpGet(BASE_API_URL + "/job/" + id);
 }
 
 
 var getTask = function (id) {
-    return httpGet(BASE_API_URL + "task/" + id)
+    return httpGet(BASE_API_URL + "/task/" + id)
     .then(function (task) {
-        return httpGet(BASE_API_URL + "task/" + id + "/status")
+        return httpGet(BASE_API_URL + "/task/" + id + "/status")
         .then(function (status) {
             task.status = status;
             return task;
@@ -54,31 +54,31 @@ var getTask = function (id) {
 
 
 var getExecution = function (id) {
-    return httpGet(BASE_API_URL + "execution/" + id)
+    return httpGet(BASE_API_URL + "/execution/" + id)
 }
 var getExecutionResult = function (id) {
-    return httpGet(BASE_API_URL + "execution/" + id + "/result")
+    return httpGet(BASE_API_URL + "/execution/" + id + "/result")
 }
 
 var getData = function (id) {
-    return httpGet(BASE_API_URL + "data/" + id)
+    return httpGet(BASE_API_URL + "/data/" + id)
 }
 
 var getProjects = function () {
-    return httpGet(BASE_API_URL + "project")
+    return httpGet(BASE_API_URL + "/project")
 }
 
 var putProject = function (project) {
-    return httpPost(BASE_API_URL + "project", project);
+    return httpPost(BASE_API_URL + "/project", project);
 }
 
 
 var putJob = function (job) {
-    return httpPost(BASE_API_URL + "job", job);
+    return httpPost(BASE_API_URL + "/job", job);
 }
 
 var putTasks = function (jobId, inputs, priority, concurrentExecutionsCount, timeout) {
-    return httpPost(BASE_API_URL + "job/" + jobId + '/tasks/' + priority + "/" + concurrentExecutionsCount + "/" + timeout, JSON.parse(inputs));
+    return httpPost(BASE_API_URL + "/job/" + jobId + '/tasks/' + priority + "/" + concurrentExecutionsCount + "/" + timeout, JSON.parse(inputs));
 }
 
 
